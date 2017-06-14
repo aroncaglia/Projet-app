3787d5904cd28dba4d648d2038f65d88af5f73ad
Robin-blc <Robin.Blanc@isep.fr>
2017-06-14 15:19:18 +0200

    liaison CGU BDD pour la deuxième fois merci Adrien

---
+++ Site/Vue/Quisommesnous.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="../Vue/CssSite1.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<meta charset="UTF-8">
+<body>
+
+
+
+<ul class="horinav">
+    <li><a href="../Vue/Acceuilavconnexion.html">Home</a></li>
+    <li><a class="active" href="Quisommesnous.php"> Qui sommes nous ? </a></li>
+    <li><a class="inscription" href="../Controleur/inscription.php">Inscription</a></li>
+    <li><a class="inscription" href="../Controleur/connexion.php">Se connecter</a></li>
+
+</ul>
+
+<div class="content">
+    <h1 class="double" > Qui sommes nous ? </h1>
+    <a class="logo"><img src="../Vue/contenu/LOGO_ISEP_FINAL.png" alt="Logo" width="63" height="61" /></a>
+    <div class="presentation">
+        <p>Domisep est une société existante depuis 5 ans qui propose d'installer un système domotique dans vos maisons.
+         Ce système est entièrement personalisable par l'utilisateur tout comme les capteurs désirés. Nous vous garantissons
+        un système sécurisé et un service après vente irréprochable pour une utilisation en tout sérennité.</p>
+    </div>
+
+
+    <div class="general">
+        <div class="information3">
+            <p>Pourquoi choisir Domisep ?</p>
+            <p>Faites nous confiance pour:</p>
+            <ul>
+                <li>La qualité de nos prestations</li>
+                <li>Les garanties de sécurité</li>
+                <li>Un service irréprochable</li>
+            </ul>
+        </div>
+
+    </div>
+</div>
+
+</body>
+
+<div class="footer">
+    <p>Copyright : B-Creative</p>
+</div>
+
+</html>
+
